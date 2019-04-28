2f3d3ddcc845966a90b14f22bd978eff207c1c83
davidvt501 <46967170+davidvt501@users.noreply.github.com>
2019-04-28 16:13:21 -0400

    Cambios de conexiones

--- includes/db_connect.php
+++ includes/db_connect.php
@@ -1,15 +1,14 @@
 <?php
-/*
+
 $host        = "host = cin.ucn.cl";
 $port        = "port = 5432";
 $dbname      = "dbname = derivaciones_db";
 $credentials = "user = derivaciones_user password=noceejif2802oicdwkcapmcsjoicej0e2iocwocdncd";
-*/
 
- $host        = "host = localhost";
+/* $host        = "host = localhost";
 $port        = "port = 5432";
 $dbname      = "dbname = derivaciones";
-$credentials = "user = postgres password=1234";
+$credentials = "user = postgres password=1234"; */
 
 $db = pg_connect( "$host $port $dbname $credentials"  );
 
